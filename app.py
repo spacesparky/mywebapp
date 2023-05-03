a0f41e3b85e2d6d4e4f795d131a11d7b250ce2f6
spacesparky <124715925+spacesparky@users.noreply.github.com>
2023-05-03 22:10:11 +0300

    Mywebapp

---
+++ app.py
@@ -0,0 +1,51 @@
+import requests
+from flask import Flask, render_template, request, redirect
+import psycopg2
+from psycopg2 import errors
+
+app = Flask(__name__)
+
+
+conn = psycopg2.connect(database="service_db",
+                        user="postgres",
+                        password="1",
+                        host="localhost",
+                        port="5432")
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+
+            return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
+    return render_template('login.html')
+
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
+
+        return redirect('/login/')
+
+    return render_template('registration.html')
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
